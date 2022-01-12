a36f924f987c58d4b8f6f6c1f0849bd8cd715806
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2022-01-12 12:18:00 +0100

    Add project files.

---
+++ Oppgave Tippekupong - kun én kamp/Match.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Oppgave_Tippekupong___kun_én_kamp
+{
+    public class Match
+    {
+        public int homeGoals = 0;
+        public int awayGoals = 0;
+        public bool matchIsRunning = true;
+
+
+
+        public void getScoreText(string bet)
+        {
+            var result = homeGoals == awayGoals ? "U" : homeGoals > awayGoals ? "H" : "B";
+            var isBetCorrect = bet.Contains(result);
+            var isBetCorrectText = isBetCorrect ? "riktig" : "feil";
+            Console.WriteLine($"Du tippet {isBetCorrectText}");
+        }
+
+        public void PrintMatch()
+        {
+            Console.Write(
+                "Kommandoer: \r\n - H = scoring hjemmelag\r\n - B = scoring bortelag\r\n - X = kampen er ferdig\r\nAngi kommando: ");
+            var command = Console.ReadLine();
+            matchIsRunning = command != "X";
+            if (command == "H") homeGoals++;
+            else if (command == "B") awayGoals++;
+            Console.WriteLine($"Stillingen er {homeGoals}-{awayGoals}.");
+        }
+    }
+}
---
+++ Oppgave Tippekupong - kun én kamp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Oppgave_Tippekupong___kun_én_kamp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var kamp = new Match();
+
+            Console.Write("Gyldig tips: \r\n - H, U, B\r\n - halvgardering: HU, HB, UB\r\n - helgardering: HUB\r\nHva har du tippet for denne kampen? ");
+            var bet = Console.ReadLine();
+         
+            while (kamp.matchIsRunning)
+            {
+                kamp.PrintMatch();
+            }
+
+          kamp.getScoreText(bet);
+        }
+
+   
+    }
+}
